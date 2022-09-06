6f3d5c6f1116b65a72757779ba6a60525a7519e8
MinoruHenrique <henrique.deaguiar.m@uni.pe>
2022-09-06 18:24:01 -0500

    Do file management

--- utils/dataaugmentation.py
+++ utils/dataaugmentation.py
@@ -20,35 +20,43 @@ class Data_Augmentation:
             img_name = IMAGE_DIR.split("/")[-1].split(".")[0]
             if (os.path.exists(os.path.join(LABEL_FOLDER, img_name+".txt"))):
                 data["image"] = cv2.imread(IMAGE_DIR)
-                data["bounding_boxes"] = self.load_label(os.path.join(LABEL_FOLDER, img_name+".txt"))
+                data["bounding_boxes"] = self.load_label(
+                    os.path.join(LABEL_FOLDER, img_name+".txt"))
             self.dataset.append(data)
 
     def load_label(self, DIR):
-        labels=[]
+        labels = []
         with open(DIR) as f:
             for line in f:
-                data_inline = line.strip(" ")
-                label={
-                    "class": data_inline[0],
+                data_inline = line.split(" ")
+                label = {
+                    "class": data_inline[0], 
                     "x_center": data_inline[1],
                     "y_center": data_inline[2],
                     "width": data_inline[3],
-                    "height": data_inline[4]
+                    "height": data_inline[4][:-1]
                 }
                 labels.append(label)
         return labels
 
-
     def save_data(self):
-        IMAGE_FOLDER = os.path.join(self.TARGET_FOLDER,"images")
+        if (not os.path.exists(self.TARGET_FOLDER)):
+            os.mkdir(self.TARGET_FOLDER)
+            os.mkdir(os.path.join(self.TARGET_FOLDER,"images"))
+            os.mkdir(os.path.join(self.TARGET_FOLDER,"labels"))
+
+        IMAGE_FOLDER = os.path.join(self.TARGET_FOLDER, "images")
         LABELS_FOLDER = os.path.join(self.TARGET_FOLDER, "labels")
         print("Saving data to " + self.TARGET_FOLDER + "...")
+
         for i, data in enumerate(self.augmented_dataset):
-            cv2.imwrite(os.path.join(IMAGE_FOLDER,str(i)+".jpg"),data["image"])
-            self.save_bb(os.path.join(LABELS_FOLDER,str(i)+".txt"),data["bounding_boxes"])
+            cv2.imwrite(os.path.join(
+                IMAGE_FOLDER, str(i)+".jpg"), data["image"])
+            self.save_bb(os.path.join(LABELS_FOLDER, str(
+                i)+".txt"), data["bounding_boxes"])
 
     def save_bb(self, DIR_NAME, labels):
-        with open(DIR_NAME, "w") as f:
+        with open(DIR_NAME, "w+") as f:
             for bb in labels:
                 line = "{} {} {} {} {}\n".format(
                     bb["class"],
@@ -58,7 +66,6 @@ class Data_Augmentation:
                     bb["height"]
                 )
                 f.write(line)
-
 
     # def load_images(self, IMAGE_FOLDER, extension):
     #     IMAGE_DIRS = glob.glob(os.path.join(IMAGE_FOLDER, "*."+extension))
@@ -71,6 +78,40 @@ class Data_Augmentation:
         gaussian_img = np.array(255*gaussian_img, dtype="uint8")
         print(gaussian_img)
         return gaussian_img
-    
-    def translation(self, img):
-        pass
+
+    def translation(self, data, x, y):
+        image = data["image"]
+        bbs = data["bounding_boxes"]
+        new_data = {}
+        # image generation
+        y_abs = int(y*image.shape[0])
+        x_abs = int(x*image.shape[1])
+        M = np.float32([
+            [1, 0, x_abs],
+            [0, 1, y_abs]
+        ])
+        new_image = cv2.warpAffine(image, M, (image.shape(1), image.shape(2)))
+        # labels transformations
+        new_bbs= []
+        for bb in bbs:
+            new_x = bb["x_center"]+x
+            new_y = bb["y_center"]+y
+
+            if(new_x>1 or new_x<0):
+                continue
+            if(new_y>1 or new_y<0):
+                continue
+
+            new_bb = {}
+            new_bb["class"] = bb["class"]
+            new_bb["x_center"] = new_x
+            new_bb["y_center"] = new_y
+            new_bb["width"] = bb["width"]
+            new_bb["height"] = bb["height"]
+
+            new_bbs.append(new_bb)
+        
+        new_data["image"] = image
+        new_data["bounding_boxe"] = new_bbs
+
+        return new_data
